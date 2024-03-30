8b942135b537ea5d9fb3e4b2c218bd7d6234e8c1
idradev <dragonfx1234@gmail.com>
2024-03-30 18:15:09 +0100

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,274 @@
+"use client";
 import Image from "next/image";
+import { useState, useEffect } from "react";
+import { animate, motion } from "framer-motion";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { faArrowUp, faArrowDown } from "@fortawesome/free-solid-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 export default function Home() {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [element1, setElement1] = useState();
+  const [element2, setElement2] = useState();
+  const [choice, setChoice] = useState(false);
+
+  const data = [
+    {
+      name: "Tha Supreme",
+      followers: 1200000,
+      image: {
+        src: "https://www.rollingstone.it/wp-content/uploads/2019/11/tha-supreme-chi-e%CC%80.jpg",
+        name: "Test",
+      },
+    },
+    {
+      name: "Fedez",
+      followers: 1200,
+      image: {
+        src: "https://www.corriere.it/methode_image/2023/11/21/Spettacoli/Foto%20Spettacoli%20-%20Trattate/344.0.900047188-kbBG-U3450673206846rkD-656x492@Corriere-Web-Sezioni.jpg",
+        name: "Test",
+      },
+    },
+    {
+      name: "Fedezzazzo",
+      followers: 15200000,
+      image: {
+        src: "https://hips.hearstapps.com/hmg-prod/images/chiara-ferragni-fedez-crisi-social-65ed8166ba6c1.jpg?crop=1xw:0.37421590909090907xh;center,top",
+        name: "Test",
+      },
+    },
+  ];
+
+  const randomIndex1 = Math.floor(Math.random() * data.length);
+  let randomIndex2 = Math.floor(Math.random() * data.length);
+  while (randomIndex2 === randomIndex1) {
+    randomIndex2 = Math.floor(Math.random() * data.length);
+  }
+
+  useEffect(() => {
+    setElement1(data[randomIndex1]);
+    setElement2(data[randomIndex2]);
+  }, []);
+
+  function setHigher() {
+    if (element2?.followers > element1?.followers) {
+      () => setChoice(true);
+    } else () => setChoice(false);
+  }
+
+  function setLower() {
+    if (element2?.followers < element1?.followers) {
+      setChoice(true);
+    } else () => setChoice(false);
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main>
+      <div className="pointer-events-none absolute z-50 w-screen flex justify-center pt-5 sm:pt-10 lg:pt-20">
+        <div className="text-center flex flex-col -space-y-1 md:space-y-0">
+          <motion.div
+            initial={{
+              y: 0,
+              opacity: 0,
+            }}
+            animate={{ y: 10, opacity: 100 }}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <h1 className="flex flex-row space-x-2 drop-shadow-md font-black text-3xl lg:text-5xl tracking-tight">
+              <FontAwesomeIcon
+                width={"2rem"}
+                color="white"
+                icon={faInstagram as IconProp}
+              />
+              <b>Chi ha più follower?</b>
+            </h1>
+          </motion.div>
+          <motion.div
+            initial={{
+              y: 10,
+              opacity: 0,
+            }}
+            animate={{ opacity: 100 }}
+            transition={{ y: 0, delay: 0.1, ease: "easeInOut" }}
+          >
+            <p className="drop-shadow-md text-lg opacity-80">Su instagram</p>
+          </motion.div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="pointer-events-none absolute z-50 w-screen flex items-center justify-center h-screen">
+        <motion.div
+          initial={{
+            opacity: 0,
+            scale: 0,
+          }}
+          animate={{
+            opacity: 100,
+            scale: 1,
+          }}
+          transition={{
+            ease: "easeInOut",
+            bounce: 5,
+          }}
+        >
+          <div className=" shadow-xl shadow-white/20 bg-white text-black rounded-full px-4 lg:px-6 py-[0.8rem] lg:py-[1.30rem]">
+            <h1 className="text-2xl lg:text-4xl">vs</h1>
+          </div>
+        </motion.div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="flex flex-col lg:flex-row h-screen">
+        {element1 && (
+          <>
+            <div className="group w-full lg:w-[50%] h-[50%] lg:h-screen relative">
+              <div className="z-10 absolute inset-0 flex flex-col justify-center items-center">
+                <motion.div
+                  initial={{
+                    opacity: 0,
+                    scale: 0,
+                  }}
+                  animate={{
+                    opacity: 100,
+                    scale: 1,
+                  }}
+                  transition={{
+                    ease: "easeInOut",
+                    bounce: 5,
+                  }}
+                >
+                  <h1 className="drop-shadow-md font-bold text-5xl lg:text-6xl">
+                    <b>{element1.name}</b>
+                  </h1>
+                </motion.div>
+                <motion.div
+                  initial={{
+                    y: -5,
+                    opacity: 0,
+                  }}
+                  animate={{ y: -4, opacity: 100 }}
+                  transition={{
+                    delay: 0.2,
+                  }}
+                >
+                  <p className="drop-shadow-md text-2xl lg:text-3xl opacity-80">
+                    ne ha
+                  </p>
+                </motion.div>
+                <motion.div
+                  initial={{
+                    y: -0,
+                    opacity: 0,
+                  }}
+                  animate={{ y: -5, opacity: 100 }}
+                  transition={{
+                    delay: 0.25,
+                  }}
+                >
+                  <h1 className="drop-shadow-md text-[#FFEA2D] font-bold text-4xl lg:text-6xl">
+                    <b>{element1.followers}</b>
+                  </h1>
+                </motion.div>
+              </div>
+              <img
+                src={element1.image.src}
+                className="w-full h-full object-cover transition duration-200 opacity-25 group-hover:lg:opacity-50"
+                draggable="false"
+                alt=""
+              />
+            </div>
+          </>
+        )}
+        {element2 && (
+          <>
+            <div className="group w-full lg:w-[50%] h-[50%] lg:h-screen relative">
+              <div className="z-10 absolute inset-0 flex flex-col justify-center items-center">
+                <motion.div
+                  initial={{
+                    opacity: 0,
+                    scale: 0,
+                  }}
+                  animate={{
+                    opacity: 100,
+                    scale: 1,
+                  }}
+                  transition={{
+                    ease: "easeInOut",
+                    bounce: 5,
+                  }}
+                >
+                  <h1 className="drop-shadow-md font-bold text-5xl lg:text-6xl">
+                    <b>{element2.name}</b>
+                  </h1>
+                </motion.div>
+                <motion.div
+                  initial={{
+                    y: -5,
+                    opacity: 0,
+                  }}
+                  animate={{ y: -0, opacity: 100 }}
+                  transition={{
+                    delay: 0.2,
+                  }}
+                >
+                  <p className="drop-shadow-md text-2xl lg:text-3xl opacity-80">
+                    ne ha
+                  </p>
+                </motion.div>
+                <div className="flex items-center pt-4 flex-col space-y-4">
+                  <motion.div
+                    initial={{
+                      y: -5,
+                      opacity: 0,
+                    }}
+                    animate={{ y: -0, opacity: 100 }}
+                    transition={{
+                      delay: 0.4,
+                    }}
+                  >
+                    <button
+                      onClick={() => setHigher()}
+                      className="shadow-md focus:outline-none w-fit text-center items-center transition duration-200 lg:hover:scale-105 lg:hover:bg-black/50 active:scale-95 focus:ring-2 focus:ring-white/10 flex flex-row space-x-2 rounded-full px-12 py-2.5 border-2 border-white bg-black/20 backdrop-blur-sm"
+                    >
+                      <span>Di più</span>{" "}
+                      <FontAwesomeIcon
+                        width={"0.8rem"}
+                        color="#54FF45"
+                        icon={faArrowUp as IconProp}
+                      />
+                    </button>
+                  </motion.div>
+                  <motion.div
+                    initial={{
+                      y: 0,
+                      opacity: 0,
+                    }}
+                    animate={{ y: -5, opacity: 100 }}
+                    transition={{
+                      delay: 0.4,
+                    }}
+                  >
+                    <button
+                      onClick={() => setLower()}
+                      className="shadow-md focus:outline-none  w-fit text-center items-center transition duration-200 lg:hover:scale-105 lg:hover:bg-black/50 active:scale-95 focus:ring-2 focus:ring-white/10 flex flex-row space-x-2 rounded-full px-10 py-2.5 border-2 border-white bg-black/20 backdrop-blur-sm"
+                    >
+                      <span>Di meno</span> {choice && <p>minchia giusto</p>}
+                      <FontAwesomeIcon
+                        width={"0.8rem"}
+                        color="#FF4545"
+                        icon={faArrowDown as IconProp}
+                      />
+                    </button>
+                  </motion.div>
+                </div>
+              </div>
+              <img
+                src={element2.image.src}
+                className="w-full h-full object-cover transition duration-200 opacity-25 group-hover:lg:opacity-50"
+                draggable="false"
+                alt=""
+              />
+            </div>
+          </>
+        )}
       </div>
     </main>
   );
