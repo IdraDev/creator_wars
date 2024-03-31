96fc1b14b1c20c2ec9333785f69768a51078781e
idradev <dragonfx1234@gmail.com>
2024-03-31 15:08:59 +0200

    Update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,9 @@
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { faTwitch } from "@fortawesome/free-brands-svg-icons";
+
 function notFound() {
   return (
     <div className="min-h-screen grid grid-cols-1 place-items-center">
@@ -9,9 +15,18 @@ function notFound() {
           La pagina che stai tentando di raggiungere non esiste o non è
           disponibile.
         </p>
-        <button className="mt-4 rounded-xl font-bold px-6 bg-white text-black py-2.5">
-          <b>Torna alla Home</b>
-        </button>
+        <Link href={"/"}>
+          <button className="flex flex-row lg:hover:scale-105 active:scale-95 space-x-2 items-center transition duration-200 mt-4 rounded-xl font-bold px-6 bg-white text-black py-2.5">
+            <span>
+              <FontAwesomeIcon
+                width={"1rem"}
+                color="black"
+                icon={faArrowLeft as IconProp}
+              />
+            </span>
+            <b>Torna alla Home</b>
+          </button>
+        </Link>
       </div>
     </div>
   );
